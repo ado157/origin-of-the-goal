93e1b7543aa826f3c64942adb242fd74fb3a6c90
ado157 <724865036@qq.com>
2025-08-08 09:16:27 +0800

    c++

---
+++ 25_08_07_01.cpp
@@ -0,0 +1,33 @@
+//#include<iostream>
+//using namespace std;
+//#include<fstream>
+//#include <string>
+////void test1() {
+////	ofstream ofs;
+////	ofs.open("test.text", ios::out);
+////	ofs << "111" << endl;
+////	ofs << "222" << endl;
+////	ofs.close();
+////}
+//void test2() {
+//	ifstream ifs;
+//	ifs.open("test.text", ios::in);
+//	if (!ifs.is_open()) {
+//		cout << "文件打开失败" << endl;
+//		return;
+//	}
+//	//char buf[1024] = { 0 };
+//	//while (ifs >> buf) {
+//	//	cout << buf<< endl;
+//	//}
+//	//while (ifs.getline(buf, sizeof(buf))) {
+//	//	cout << buf << endl;
+//	//}
+//	string buf;
+//	while (getline(ifs,buf)) {
+//		cout << buf << endl;
+//	}
+//}
+//int main() {
+//	test2();
+//}
