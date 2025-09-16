ec4ca4017bc274d1363da1179df03e90c9f75464
ado157 <724865036@qq.com>
2025-09-16 13:45:07 +0800

    Add files via upload

---
+++ 25_09_15_03.cpp
@@ -0,0 +1,26 @@
+//#include<iostream>
+//#include<string>
+//using namespace std;
+//void test1() {
+//	string str1 = "ÎÒ";
+//	str1 += "ÍæÓÎÏ·";
+//	cout << "str1 : " << str1 << endl;
+//	str1 += ':';
+//	cout << "str1 : " << str1 << endl;
+//	string str2 = "bd3";
+//	str1 += str2;
+//	cout << "str1 : " << str1 << endl;
+//	string str3 = "I";
+//	str3.append(" love ");
+//	cout << "str3: " << str3 << endl;
+//	str3.append("game abcde", 4);
+//	cout << "str3: " << str3 << endl;
+//	//str3.append(str2);
+//	//cout << "str3: " << str3 << endl;
+//	str3.append(str2,0,2);
+//	cout << "str3: " << str3 << endl;
+//
+//}
+//int main() {
+//	test1();
+//}
